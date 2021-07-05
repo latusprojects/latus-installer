37ff5525ba27738e4a3ac4776ea9fbfe6b1de30a
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-05 04:20:08 +0200

    Add Installer.php, update InstallCommand.php and add "jackiedo/dotenv-editor" as a dependency to composer.json

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -5,6 +5,7 @@ namespace Latus\Installer\Console\Commands;
 
 
 use Illuminate\Console\Command;
+use Latus\Installer\Installer;
 
 class InstallCommand extends Command
 {
@@ -32,6 +33,67 @@ class InstallCommand extends Command
         parent::__construct();
     }
 
+    protected function askDatabaseDetails(): array
+    {
+
+        $details = [
+            'driver' => $this->ask('Driver', 'mysql'),
+            'host' => $this->ask('Host', 'localhost'),
+            'username' => $this->ask('Username'),
+            'database' => $this->ask('Database'),
+            'password' => $this->ask('Password'),
+            'port' => $this->ask('Port', 3306),
+            'prefix' => $this->ask('Prefix', ''),
+        ];
+
+        try {
+            Installer::verifyDatabaseDetails($details);
+        } catch (\InvalidArgumentException $e) {
+            $this->error($e->getMessage());
+            $this->askDatabaseDetails();
+        }
+
+        return $details;
+    }
+
+    protected function askUserDetails(): array
+    {
+
+        $details = [
+            'username' => $this->ask('Username', 'admin'),
+            'email' => $this->ask('Email'),
+            'password' => $this->secret('Password'),
+            'password_confirmation' => $this->secret('Confirm Password'),
+        ];
+
+        try {
+            Installer::tryUserDetails($details);
+        } catch (\InvalidArgumentException $e) {
+            $this->error($e->getMessage());
+            $this->askUserDetails();
+        }
+
+        return $details;
+    }
+
+    protected function askAppDetails(): array
+    {
+
+        $details = [
+            'name' => $this->ask('Name'),
+            'url' => $this->ask('App-URL'),
+        ];
+
+        try {
+            Installer::tryAppDetails($details);
+        } catch (\InvalidArgumentException $e) {
+            $this->error($e->getMessage());
+            $this->askAppDetails();
+        }
+
+        return $details;
+    }
+
     /**
      * Execute the console command.
      *
@@ -39,6 +101,26 @@ class InstallCommand extends Command
      */
     public function handle()
     {
+
+        $this->info('Welcome! This CLI will guide you through the installation of latus. This won\'t take long, I promise! ');
+
+        $this->warn('#1 - Database Details');
+        $database_details = $this->askDatabaseDetails();
+
+        $this->warn('#2 - Admin Account');
+        $user_details = $this->askUserDetails();
+
+        $this->warn('#3 - Application');
+        $app_details = $this->askAppDetails();
+
+        $installer = new Installer($database_details, $user_details, $app_details);
+        $installer->setCli($this);
+
+        try {
+            $installer->commenceInstallation();
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
         return 0;
     }
 }
---
+++ src/Installer.php
@@ -0,0 +1,183 @@
+<?php
+
+
+namespace Latus\Installer;
+
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+use Jackiedo\DotenvEditor\Facades\DotenvEditor;
+use Symfony\Component\Console\Command\Command;
+
+class Installer
+{
+
+    protected \Illuminate\Console\Command|null $command = null;
+
+    public function __construct(
+        protected array $database_details,
+        protected array $user_details,
+        protected array $app_details,
+    )
+    {
+    }
+
+    protected const DATABASE_DETAILS_VALIDATION_RULES = [
+        'host' => 'required|string|min:5',
+        'username' => 'required|string|min:3|max:16',
+        'database' => 'required|string|min:3|max:54',
+        'password' => 'required|string|min:6|max:32',
+        'port' => 'required|integer|min:0|max:65535',
+        'driver' => 'required|in:mysql,postgres,sqlite,sqlsrv',
+        'prefix' => 'sometimes|string|max:10',
+    ];
+
+    protected const APP_DETAILS_VALIDATION_RULES = [
+        'name' => 'required|string|min:3|max:255',
+        'url' => 'required|url'
+    ];
+
+    protected const USER_DETAILS_VALIDATION_RULES = [
+        'username' => 'required|min:5|max:50',
+        'email' => 'required|email',
+        'password' => 'required|string|min:8|max:255|confirmed',
+        'password_confirmation' => 'required'
+    ];
+
+    public static function isInstalled(): bool
+    {
+        return stream_resolve_include_path(base_path('.installed')) !== false;
+    }
+
+    public static function setIsInstalled(bool $value)
+    {
+        if ($value) {
+            File::put(base_path('.installed'), '');
+            return;
+        }
+        if (self::isInstalled()) {
+            File::delete(base_path('.installed'));
+        }
+    }
+
+    protected static function setInstallerDatabaseConfig(array $values)
+    {
+        Config::set('database.connections.latus_installer', [
+            'driver' => $values['driver'],
+            'host' => $values['host'],
+            'database' => $values['database'],
+            'username' => $values['username'],
+            'password' => $values['password'],
+            'port' => $values['port'],
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => $values['prefix'],
+            'strict' => false,
+        ]);
+    }
+
+    protected static function validateValuesWithRules(array $values, array $rules): bool
+    {
+        $validator = Validator::make($values, $rules);
+
+        if ($validator->fails()) {
+            throw new \InvalidArgumentException($validator->errors()->first());
+        }
+
+        return true;
+    }
+
+    public static function tryUserDetails(array $values): bool
+    {
+        return self::validateValuesWithRules($values, self::USER_DETAILS_VALIDATION_RULES);
+    }
+
+    public static function tryAppDetails(array $values): bool
+    {
+        return self::validateValuesWithRules($values, self::APP_DETAILS_VALIDATION_RULES);
+    }
+
+    public static function verifyDatabaseDetails(array $values): bool
+    {
+        self::validateValuesWithRules($values, self::DATABASE_DETAILS_VALIDATION_RULES);
+
+        self::setInstallerDatabaseConfig($values);
+
+        if (DB::connection('latus_installer')->getDatabaseName()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function runMigrations()
+    {
+        $this->printToConsole('Running migrations...');
+
+        match (Artisan::call('migrate:refresh')) {
+            Command::FAILURE => throw new \Exception('Migration failed. Please verify that you are using a database version supported by Laravel.'),
+            Command::INVALID => throw new \Exception('Migration failed. There were invalid migrations. Please verify that all external packages\'s migrations are valid.'),
+            default => 0
+        };
+
+        $this->printToConsole('Migrations complete!');
+    }
+
+    protected function updateEnvironment()
+    {
+        $this->printToConsole('Updating environment...');
+
+        DotenvEditor::setKeys([
+            'APP_NAME' => $this->app_details['name'],
+            'APP_URL' => $this->app_details['url'],
+
+            'DB_CONNECTION' => $this->database_details['driver'],
+            'DB_HOST' => $this->database_details['host'],
+            'DB_PORT' => $this->database_details['port'],
+            'DB_DATABASE' => $this->database_details['database'],
+            'DB_USERNAME' => $this->database_details['username'],
+            'DB_PASSWORD' => $this->database_details['password'],
+            'DB_DRIVER' => $this->database_details['driver'],
+            'DB_PREFIX' => $this->database_details['prefix'],
+        ]);
+
+        DotenvEditor::save();
+
+        $this->printToConsole('Environment updated!');
+    }
+
+    protected function tryDetails()
+    {
+        self::tryAppDetails($this->app_details);
+        self::tryUserDetails($this->user_details);
+        self::verifyDatabaseDetails($this->database_details);
+    }
+
+    public function commenceInstallation()
+    {
+        $this->tryDetails();
+
+        $this->updateEnvironment();
+
+        $this->runMigrations();
+    }
+
+    protected function printToConsole(string $message, string $type = 'info')
+    {
+        if ($this->command !== null) {
+            match ($type) {
+                'warn' => $this->command->warn($message),
+                'error' => $this->command->error($message),
+                default => $this->command->info($message),
+            };
+        }
+    }
+
+    public function setCli(\Illuminate\Console\Command $command)
+    {
+        $this->command = $command;
+    }
+}
