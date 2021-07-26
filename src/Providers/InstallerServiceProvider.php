9b33af150a9a9822b2417acb91a88c2817295bf8
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-26 20:22:23 +0200

    Add database-seeders

--- src/Providers/InstallerServiceProvider.php
+++ src/Providers/InstallerServiceProvider.php
@@ -4,10 +4,14 @@ namespace Latus\Installer\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Latus\Installer\Console\Commands\InstallCommand;
+use Latus\Installer\Database\Seeders\DatabaseSeeder;
 use Latus\Installer\Installer;
+use Latus\Installer\Providers\Traits\RegistersSeeders;
 
 class InstallerServiceProvider extends ServiceProvider
 {
+    use RegistersSeeders;
+
     /**
      * Register services.
      *
@@ -17,6 +21,10 @@ class InstallerServiceProvider extends ServiceProvider
     {
         $this->commands([
             InstallCommand::class,
+        ]);
+
+        $this->registerSeeders([
+            DatabaseSeeder::class,
         ]);
     }
 
