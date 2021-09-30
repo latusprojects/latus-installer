44de25eeedaa99fbd10ded292472414ceeb32189
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-30 21:02:58 +0200

    Register listeners for plugin, theme and repository related events

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -3,8 +3,14 @@
 namespace Latus\Installer\Providers;
 
 use Latus\Installer\Events\AppDetailsProvided;
+use Latus\Installer\Events\InstallableComposerRepositoryProvided;
+use Latus\Installer\Events\InstallablePluginProvided;
+use Latus\Installer\Events\InstallableThemeProvided;
 use Latus\Installer\Events\UserDetailsProvided;
 use Latus\Installer\Listeners\CreateDefaultUser;
+use Latus\Installer\Listeners\InstallComposerRepository;
+use Latus\Installer\Listeners\InstallPlugin;
+use Latus\Installer\Listeners\InstallTheme;
 use Latus\Installer\Listeners\SetupApp;
 
 class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
@@ -15,6 +21,15 @@ class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
         ],
         AppDetailsProvided::class => [
             SetupApp::class
-        ]
+        ],
+        InstallableThemeProvided::class => [
+            InstallTheme::class
+        ],
+        InstallablePluginProvided::class => [
+            InstallPlugin::class
+        ],
+        InstallableComposerRepositoryProvided::class => [
+            InstallComposerRepository::class
+        ],
     ];
 }
