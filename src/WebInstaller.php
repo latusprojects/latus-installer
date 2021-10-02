dd85a5578c22d9ccbcad4752f68d8885fbc24d21
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-02 13:19:40 +0200

    Move methods from WebInstaller.php to Installer.php

--- src/WebInstaller.php
+++ src/WebInstaller.php
@@ -2,42 +2,6 @@
 
 namespace Latus\Installer;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
-
-class WebInstaller
+class WebInstaller extends Installer
 {
-
-    protected function setTemporaryConnectionDetails(array $details)
-    {
-        Config::set('database.connections.latus_installer', [
-            'driver' => $details['driver'],
-            'host' => $details['host'],
-            'database' => $details['database'],
-            'username' => $details['username'],
-            'password' => $details['password'],
-            'port' => $details['port'],
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => $details['prefix'],
-            'strict' => false,
-        ]);
-    }
-
-    /**
-     * Attempt a database connection
-     *
-     * @param array $details
-     * @return bool
-     */
-    public function attemptConnectionWithDetails(array $details): bool
-    {
-        $this->setTemporaryConnectionDetails($details);
-
-        if (!DB::connection('latus_installer') || !DB::connection('latus_installer')->getDatabaseName()) {
-            return false;
-        }
-
-        return true;
-    }
 }
