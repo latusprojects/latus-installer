4f6c653a12c349118a0fc9b8f6e866a6eb933438
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-02 11:35:03 +0200

    Register additional listeners

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -3,15 +3,20 @@
 namespace Latus\Installer\Providers;
 
 use Latus\Installer\Events\AppDetailsProvided;
+use Latus\Installer\Events\DatabaseDetailsProvided;
+use Latus\Installer\Events\DefaultUserCreated;
 use Latus\Installer\Events\InstallableComposerRepositoryProvided;
 use Latus\Installer\Events\InstallablePluginProvided;
 use Latus\Installer\Events\InstallableThemeProvided;
 use Latus\Installer\Events\UserDetailsProvided;
+use Latus\Installer\Listeners\AssignRoleToDefaultUser;
 use Latus\Installer\Listeners\CreateDefaultUser;
 use Latus\Installer\Listeners\InstallComposerRepository;
 use Latus\Installer\Listeners\InstallPlugin;
 use Latus\Installer\Listeners\InstallTheme;
-use Latus\Installer\Listeners\SetupApp;
+use Latus\Installer\Listeners\SetTemporaryDatabaseConfig;
+use Latus\Installer\Listeners\UpdateAppDetailsInEnvFile;
+use Latus\Installer\Listeners\UpdateDatabaseDetailsInEnvFile;
 
 class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
 {
@@ -20,7 +25,11 @@ class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
             CreateDefaultUser::class
         ],
         AppDetailsProvided::class => [
-            SetupApp::class
+            UpdateAppDetailsInEnvFile::class
+        ],
+        DatabaseDetailsProvided::class => [
+            UpdateDatabaseDetailsInEnvFile::class,
+            SetTemporaryDatabaseConfig::class,
         ],
         InstallableThemeProvided::class => [
             InstallTheme::class
@@ -31,5 +40,8 @@ class EventServiceProvider extends \Illuminate\Events\EventServiceProvider
         InstallableComposerRepositoryProvided::class => [
             InstallComposerRepository::class
         ],
+        DefaultUserCreated::class => [
+            AssignRoleToDefaultUser::class
+        ]
     ];
 }
