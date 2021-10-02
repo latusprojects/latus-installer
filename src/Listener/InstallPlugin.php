10df4eaef503412c75a9fc7b6a5a256f68b488b8
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-10-02 14:03:29 +0200

    Update InstallTheme.php and InstallPlugin.php

--- src/Listener/InstallPlugin.php
+++ src/Listener/InstallPlugin.php
@@ -3,18 +3,31 @@
 namespace Latus\Installer\Listeners;
 
 use Latus\Installer\Events\InstallablePluginProvided;
+use Latus\Installer\Listeners\Traits\InstallsPackage;
+use Latus\Plugins\Exceptions\ComposerCLIException;
+use Latus\Plugins\Models\Plugin;
 use Latus\Plugins\Services\PluginService;
 
 class InstallPlugin
 {
+    use InstallsPackage;
+
     public function __construct(
         protected PluginService $pluginService,
     )
     {
     }
 
+    /**
+     * @param InstallablePluginProvided $event
+     * @throws ComposerCLIException
+     */
     public function handle(InstallablePluginProvided $event)
     {
-        $this->pluginService->createPlugin($event->pluginData);
+        /**
+         * @var Plugin $plugin
+         */
+        $plugin = $this->pluginService->createPlugin($event->pluginData);
+        $this->runComposer($plugin);
     }
 }
--- src/Listener/InstallTheme.php
+++ src/Listener/InstallTheme.php
@@ -3,18 +3,31 @@
 namespace Latus\Installer\Listeners;
 
 use Latus\Installer\Events\InstallableThemeProvided;
+use Latus\Installer\Listeners\Traits\InstallsPackage;
+use Latus\Plugins\Exceptions\ComposerCLIException;
+use Latus\Plugins\Models\Theme;
 use Latus\Plugins\Services\ThemeService;
 
 class InstallTheme
 {
+    use InstallsPackage;
+
     public function __construct(
         protected ThemeService $themeService,
     )
     {
     }
 
+    /**
+     * @param InstallableThemeProvided $event
+     * @throws ComposerCLIException
+     */
     public function handle(InstallableThemeProvided $event)
     {
-        $this->themeService->createTheme($event->themeData);
+        /**
+         * @var Theme $theme
+         */
+        $theme = $this->themeService->createTheme($event->themeData);
+        $this->runComposer($theme);
     }
 }
